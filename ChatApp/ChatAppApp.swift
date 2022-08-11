6edc2a1b4ce596c01fa654c9c6f2e49b60ce57d0
k2tam <buingockhaitam01@gmail.com>
2022-08-11 15:19:19 +0700

    Initial Commit

---
+++ ChatApp/ChatAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatAppApp.swift
+//  ChatApp
+//
+//  Created by k2 tam on 11/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct ChatAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
